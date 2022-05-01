eb2114b3eb7060dc8bf9e79f34c285d9d999a524
alkafoy <esergey05@gmail.com>
2022-05-01 13:56:42 +0300

    add method getCallerClassAndMethodName

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,17 @@
-import java.math.*;
-import java.util.Arrays;
-import java.util.function.*;
-
 public class Main {
-    public static void main(String[] args) throws Exception {
-        test();
+    public static void main(String[] args) {
+        System.out.println(getCallerClassAndMethodName());
+        anotherMethod();
+    }
+
+    private static void anotherMethod() {
+        System.out.println(getCallerClassAndMethodName());
     }
 
     public static String getCallerClassAndMethodName() {
-
-        return null; // your implementation here
-    }
-
-    public static void test() {
         Thread current = Thread.currentThread();
         StackTraceElement[] methods = current.getStackTrace();
 
-        for (var info : methods)
-            System.out.println(info);
+        return null; // your implementation here
     }
-
 }
-
-
